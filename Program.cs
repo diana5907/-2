2db27da4b20a7b998803ce4d311af7088f5f9b36
diana5907 <127054819+diana5907@users.noreply.github.com>
2024-04-09 15:31:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace FunctionCalculation
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a = 1.25;  // Початкове значення x
+            double b = 6.75;  // Кінцеве значення x
+            double dx = 0.25; // Крок зміни x
+
+            Console.WriteLine("Результати обчислення функції y = √x * 4 на проміжку [{0}, {1}] з кроком {2}", a, b, dx);
+            Console.WriteLine("x\t\t| y");
+            Console.WriteLine("--------------------");
+
+            // Початок оператора циклу з передумовою
+            for (double x = a; x <= b; x += dx)
+            {
+                double y = Math.Sqrt(x) * 4; // Обчислення значення функції y = √x * 4
+                y = Math.Round(y, 2); // Округлення значення до двох знаків після коми
+
+                Console.WriteLine("{0}\t\t| {1}", Math.Round(x, 2), y); // Округлення x та виведення значень
+            }
+            // Кінець оператора циклу з передумовою
+
+            Console.ReadLine(); // Чекаємо на натискання Enter перед завершенням програми
+        }
+    }
+}
